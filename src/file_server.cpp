8f339a73a1c1c427fdd75d08948ccf93c1670fa1
Sam Wilcock <sam.wilcock@polimi.it>
2024-08-06 01:06:34 +0200

    Migrating

--- src/file_server.cpp
+++ src/file_server.cpp
@@ -1,38 +1,29 @@
 /*
-© Siemens AG, 2017-2018
-Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-<http://www.apache.org/licenses/LICENSE-2.0>.
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Author: Sam Wilcock (sam.wilcock@polimi.it)
 */
 
-#include <ros/ros.h>
-#include <ros/package.h>
-#include <file_server/GetBinaryFile.h>
+#include "rclcpp/rclcpp.hpp"
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <ros2_file_server/srv/get_binary_file.hpp>
 #include <fstream>
 
-bool get_file(
-	file_server::GetBinaryFile::Request &req,
-	file_server::GetBinaryFile::Response &res)
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("file_server");
+
+
+void get_file(
+	const std::shared_ptr<ros2_file_server::srv::GetBinaryFile::Request> req,
+	std::shared_ptr<ros2_file_server::srv::GetBinaryFile::Response> res)
 {
 
 	// analyze request:
-	if (req.name.compare(0,10,"package://")!=0)
+	if (req->name.compare(0,10,"package://")!=0)
 	{
-		ROS_INFO("only \"package://\" adresses allowed.");
-		return true;
+		RCLCPP_INFO(LOGGER, "only \"package://\" adresses allowed.");
 	}	
-	std::string adress = req.name.substr(10);
+	std::string adress = req->name.substr(10);
 	std::string package = adress.substr(0,adress.find("/"));
 	std::string filepath = adress.substr(package.length());
-	std::string directory = ros::package::getPath(package);
+	std::string directory = ament_index_cpp::get_package_share_directory(package);
 	directory+=filepath;
 	
 	// open file:
@@ -41,29 +32,29 @@ bool get_file(
 	// stop if file does not exist:
 	if(!inputfile.is_open())
 	{
-		ROS_INFO("file \"%s\" not found.\n", req.name.c_str());
-		return true;
+		RCLCPP_INFO(LOGGER, "file \"%s\" not found.\n", req->name.c_str());
 	}
 
 	// read file contents:	
-	res.value.assign(
+	res->value.assign(
 		(std::istreambuf_iterator<char>(inputfile)),
 		std::istreambuf_iterator<char>());
 
-	ROS_INFO("get_file request: %s\n", req.name.c_str());
-	//ROS_INFO("package: %s\n",package.c_str());
-	//ROS_INFO("filepath: %s\n",filepath.c_str());
-	//ROS_INFO("directory: %s\n",directory.c_str());
-
-	return true;
+	RCLCPP_INFO(LOGGER, "get_file request: %s\n", req->name.c_str());
+	RCLCPP_INFO(LOGGER, "package: %s\n",package.c_str());
+	RCLCPP_INFO(LOGGER, "filepath: %s\n",filepath.c_str());
+	RCLCPP_INFO(LOGGER, "directory: %s\n",directory.c_str());
 }
+
+
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "file_server");
-	ros::NodeHandle n;
-	ros::ServiceServer serviceServer = n.advertiseService("/file_server/get_file", get_file);
-	ROS_INFO("ROSbridge File Server initialized.");
-	ros::spin();
+	rclcpp::init(argc, argv);
+	auto node = rclcpp::Node::make_shared("file_server");
+	rclcpp::Service<ros2_file_server::srv::GetBinaryFile>::SharedPtr service = 
+		node->create_service<ros2_file_server::srv::GetBinaryFile>("/file_server/get_file", &get_file);
+	RCLCPP_INFO(LOGGER, "ROSbridge File Server initialized.");
+	rclcpp::spin(node);
 
 	return 0;
 }
